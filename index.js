1b4b9d30fed2d3e6c855577ee6b6541bb9d3ef2c
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2025-06-28 15:24:54 +0530

    basic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const formRoutes = require('./routes/formRoutes');
-const prerender = require('prerender-node');
+// const prerender = require('prerender-node');
 require('dotenv').config();
 const path = require('path');
 
@@ -22,25 +22,25 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 
-app.use(
-  prerender
-    .set('prerenderToken', 'h6hlaIEOJH9gBtlmTbr6')
-    // .whitelisted(['/form', '/list']) 
-);
+// app.use(
+//   prerender
+//     .set('prerenderToken', 'h6hlaIEOJH9gBtlmTbr6')
+//     // .whitelisted(['/form', '/list']) 
+// );
 
 // API Routes
 app.use('/api', formRoutes);
 
 // Basic route
-app.get(/^\/(?!api).*/, (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 // Serve frontend build
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.get(/^\/(?!api).*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
 
 
 // Connect to DB and start server once DB is connected
